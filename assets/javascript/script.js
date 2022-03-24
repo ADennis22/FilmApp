72495746a126adbcab687e96813d1295d88912a0
unknown <austin.w.dennis22@gmail.com>
2022-03-23 20:22:43 -0500

    added variables and eventlisteners to our fetch request

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,15 @@
-
-fetch('https://omdbapi.com/?apikey=dc3a7f27&s=Lion%20king')
+var searchButton = document.querySelector("#searchButton");
+var searchBar = document.querySelector("#searchBar")
+console.log(searchButton)
+searchButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    var search = searchBar.value
+    console.log(search)
+    fetch('https://omdbapi.com/?apikey=dc3a7f27&t=' + search)
     .then(response => response.json())
-    .then(data => console.log(data));
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+});
+
+
+
